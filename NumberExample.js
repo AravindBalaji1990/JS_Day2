8ce7bd7a69cb38023095c2555062bd394f654e3a
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-02-27 20:38:07 +0300

    test

--- NumberExample.js
+++ NumberExample.js
@@ -1,4 +1,4 @@
-//representation
+// representation
 // var x=102;//integer value  
 // console.log(x)
 // let y=102.7;//floating point value  
@@ -6,19 +6,19 @@
 // var z=13e4;//exponent value, output: 130000  
 // console.log(z)
 // var n=new Number(16);//integer value by number object  
-// console.log(n.valueOf)
+// console.log(n)
 
 //fixed
 // var a=98.9721;  
 // console.log(a.toFixed()); // rounding to nearest value 
 
-var b=98.9721;  
-console.log(typeof b)
-console.log(b.toFixed(2)); // correcting it to 2 decimal places 
+// var b=98.9721;  
+// console.log(typeof b)
+// console.log(b.toFixed(2)); // correcting it to 2 decimal places 
 
-// //precision
- var c=98.1726546E33;  
-console.log(c.toPrecision(2));  
+// // // //precision
+//  var c=98.1726546E33;  
+// console.log(c.toPrecision(2));  
 
 //1234567890123456789012345678901234567890
                         //1234567890123456789012345678901234567890n
@@ -26,8 +26,10 @@ console.log(c.toPrecision(2));
 // console.log(sampple)
 
 //conversion of anything to numbers
-var a="50";  
+var a="50"; 
 var b='50.25'
+var data = Number.parseInt(a)+ Number.parseInt(b)
+console.log(data)
 var c="String";  
 var d="String50";  
 var e="50.75String" 
@@ -38,4 +40,5 @@ console.log(Number.parseInt(c));
 console.log(Number.parseInt(d));  
 console.log(Number.parseInt(e));
 console.log(Number.parseInt(f));
+
 
--- StringExample.js
+++ StringExample.js
@@ -11,43 +11,43 @@
 
 
 // substr - getting the part of string with a string
-// var str=123;  
-// console.log(typeof str.toString())
+var str=123;  
+console.log(typeof str.toString())
 // var num =123
 // console.log(typeof num)
 // console.log(parseInt(str))
 // console.log(parseFloat(str))
 
-// var numvalue = Number(str)
-// console.log(parseInt(numvalue))
-// console.log(str.substr(5,5));// deprecated  
+
+var str="Javascript";  
+console.log(str.substr(5,5));// deprecated  
 //new way
        // 0123 
 // var str="Javascript";  
-// console.log(str.substring(0,4));  // 0,1,2,3
-// console.log(str.length); 
-// console.log(str.toLowerCase());  
-// console.log(str.toUpperCase());  
-// console.log(str.startsWith('Ja'));
-// console.log(str.endsWith('pt'));
-// console.log(str == 'Javascript'); // case sensitivity
-// console.log(5 == '5'); 
+console.log(str.substring(0,4));  // 0,1,2,3
+console.log(str.length); // get the lenght of the word
+console.log(str.toLowerCase());  // conver t the data to lower case
+console.log(str.toUpperCase());  // convert the data to upper case
+console.log(str.startsWith('Ja')); // check whether the data starts with
+console.log(str.endsWith('pt'));// check whether the data ends with
+console.log(str == 'Javascript'); // case sensitivity
+
  
 
 // console.log(str === 'Javascript'); // strict equality operator 
 // console.log(5 === "5")
-// var x = 5
+var x = 5
 // console.log(Number.parseInt("5xyz"))// it extract the integer value and converts to Number
 // console.log(x == Number.parseInt("5xyz")) // compare justr the value
-// console.log(x === "5") // compare the value and dataype of the value
+console.log(x == "5") // compare the value and dataype of the value
 // console.log(x === Number.parseInt("5xyz")) // compare the value and dataype of the value
 // console.log(typeof 5)
 // console.log(typeof "5")
 
 //match - match regular expression 
-// var str="abcd1234+";  
-// const result = str.match(/([a-zA-Z]+)([0-9]+)(\+)/)
-// console.log(result);  
+var str="abcd1234+";  
+const result = str.match(/([a-zA-Z]+)([0-9]+)(\+)/)
+console.log(result);  
 
 // var data = "hello world";
 // const result1 = /world/;
@@ -76,11 +76,11 @@
 // console.log(str.slice(-5,-1));  
 
 //search - which index the word exists it will give as output
-var str="JavaScript is a scripting language. Scripting languages are often interpreted";  
-console.log(str.search("xyz"));  
+// var str="JavaScript is a scripting language. Scripting languages are often interpreted";  
+// console.log(str.search("xyz"));  
 
-var str="JavaScript is a scripting language. Scripting languages are often interpreted";  
-console.log(str.search(/Scripting/));  
+// var str="JavaScript is a scripting language. Scripting languages are often interpreted";  
+// console.log(str.search(/Scripting/));  
 
 //parse - converting form one type to another
 // var data =100;
