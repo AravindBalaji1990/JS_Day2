db7daa38d76c67df984afdba6257e5cbd10f154a
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-04-05 19:31:28 +0300

    new test

--- ArrayMethods1.js
+++ ArrayMethods1.js
@@ -7,28 +7,49 @@
 // console.log(arr2);  
 
 // copy with in 
-//      [0          ,1        ,       2,        3 ]        
-var arr=["AngularJS","Node.js","JQuery","Bootstrap"] 
-// var arr=["Node.js","JQuery","JQuery","Bootstrap"]  -> Output
-// place at 0th position, the element between 1st and 2nd position. 
-// copies the elemtn between source and target 
-//copywithin(target, startindex, endindex) -> target will be replaced by startindex
-var result=arr.copyWithin(0,1,3);  
-console.log(result);  
-//         0         , 1        ,  2    ,   3
-//  var arr=["AngularJS","Node.js","JQuery","Bootstrap"];  
-// // place from 1st position, the elements after 2nd position.  
+//           0,1,2,3,4,5
+//Example - [1,2,3,4,5,6]
+// target index -3
+// startindex -0
+// end index -2
+//        0,1
+// [1,2,3,1,2,6]
 
-// var result=arr.copyWithin(1,2);  
-// console.log(result);  
+//      [0          ,1        ,       2,        3 ]        
+// var arr=["AngularJS","Node.js","JQuery","Bootstrap"] 
+// // var arr=["Node.js","JQuery","JQuery","Bootstrap"] // -> Output
+// // place at 0th position, the element between 1st and 2nd position. 
+// // copies the elemtn between source and target 
+// // copywithin(target, startindex, endindex) -> target will be replaced by startindex
+// var result=arr.copyWithin(0,1,3);  
+// console.log(result);   
+// //         0         , 1        ,  2    ,   3
+//  var arr=["AngularJS","Node.js","JQuery","Bootstrap"]; 
+//  // copywithin(target, startindex) there is no end index available so it replaces everything after the start index
+//  var result=arr.copyWithin(1,2);  
+// console.log(result);   
+// place from 1st position, the elements after 2nd position.  
+
+
 
 //         0        , 1      , 2       , 3
 // var arr=["AngularJS","Node.js","JQuery","Bootstrap"];  
-// var arr=["AngularJS","Node.js","AngularJS","Node.js"];  
+// // var arr=["AngularJS","Node.js","AngularJS","Node.js"];  
 // // place from 2nd position, the elements after 0th position.  
 // var result=arr.copyWithin(2);  
 // console.log(result);  
 
+// what does of in for loop does
+// var arr=['John','Michael','Embrose','Herry','Lewis'];  
+// for(arr1 of arr){
+//     console.log("values from array usinf of ", arr1)
+// }
+
+// // what does 'in' in for loop does
+// var arr=['John','Michael','Embrose','Herry','Lewis'];  
+// for(arr1 in arr){
+//     console.log("values from array using in ", arr1)
+// }
 // entries -> it gets the index & values in the array as a key-value pair 
 //        0       1        2          3       4
 // var arr=['John','Michael','Embrose','Herry','Lewis'];  
@@ -43,7 +64,7 @@ console.log(result);
 // every  - where all the elements inside the array are put to test 
 // its returns wither true/false
 // to validate agians the function
-// var marks=[0,6,2];  
+// var marks=[0,2,1];  
 // // // function with argument and return type
 // // // named fucntion wiht argument/parameter
 // function check(value)  
@@ -53,16 +74,18 @@ console.log(result);
 // console.log(marks.every(check));  
 
 // //flat
+// var arr =['a','b','c']
 // var arr=['a','b',['c','d']]; //given 2D array  
 // var newArr=arr.flat(); //using flat() method  
-// console.log("After flattening the array:  "+ newArr);  
+// console.log("After flattening the array:  "+ typeof newArr);  
+// console.log("After flattening the array:  "+  newArr);  
 
 // var arr=[90,18,[89,56],[13,20,[67,17]]]; //given multidimensional array  
 // var newArr=arr.flat(1); //using flat() method  
 // console.log("After flattening the array:  "+newArr);  
 
-var arr =[1,2,[3,4,[5,6]]]
-console.log(arr.flat(Infinity))
+// var arr =[1,2,[3,4,[5,6]]]
+// console.log(arr.flat(Infinity))
 // default 1
 // var arr=[90,18,[13,20],[67,17,[56,45]]]; //given multidimensional array  
 // var newArr=arr.flat(); //using flat() method with a specified depth value.  
@@ -77,7 +100,7 @@ console.log(arr.flat(Infinity))
 // fill the data from the nth position
 //fill("value",startindex, end index)
 //if end index is not available then replace value till the end index
-// var result=arr.fill("Bootstrap",2);  
+// var result=arr.fill("Bootstrap",1);  
 // console.log(arr);  
 
 //Array.prototype.filter -> exact result
@@ -90,6 +113,8 @@ console.log(arr.flat(Infinity))
 // {  
 //   return marks>30;    
 // }  
+// // which are the statements that will be true will be filtered out 
+// // exact result not a true or false
 // console.log(marks.filter(checkdata));  
 
 // function test(element, index, array) {  
@@ -109,17 +134,20 @@ console.log(arr.flat(Infinity))
 // but it will show the result at the first hit 
 // it will not go beyond that 
 // it will stop to execute as soon as the condition is satisfied but it will not iterate through out the array
-// var result=arr.find(x => x>20);  
-// console.log(result)  
+//  var arr=[5,19,25,34];  
+// var result1=arr.find(x => x>20);  
+// console.log(result1)  
 
+// var result2=arr.findIndex(x => x>20);  
+// console.log(result2)  
 //find index - similar to the find
 // but it pull the idex value instead of the exact value
 //      [0,1,2, 3, 4, 5]
 // var arr=[25,1,19,5,4,22];  
-//find INdex will have a logic and check for the validariton of the logic
-//out put will be the index value of the satisfied condition
+// //find INdex will have a logic and check for the validariton of the logic
+// //out put will be the index value of the satisfied condition
 // arr.sort((a,b) =>a-b) //ascending order 
-
+// console.log(arr)
 // arr.sort((a,b) => b-a) //descending order 
 // console.log(arr)
 // var result=arr.findIndex(x=>x>20);  
@@ -155,10 +183,26 @@ console.log(arr.flat(Infinity))
 // arr.push("JQuery","Bootstrap");  
 // console.log("Length after invoking push(): "+arr.length);   
 // console.log("Update array: "+arr); 
-// arr.unshift("vuejs") 
+// arr.unshift("vuejs") // put the value in front unshift
 // console.log("Update array: "+arr); 
 
-//slice - mean
+//splice
+
+// var arr = [1,2,3,4,5]
+
+// var randomindexinsert = Math.floor(Math.random() * (arr.length-1))
+
+// arr.splice(randomindexinsert,0, 101)
+// console.log(arr)
+
+//           0     1     2    3     4
+// var arr1 =['abc','def','ghi','jkl','mno']
+// //splice - arr.splice(startindex,deletecount(optional), item1,item2)
+// arr1.splice(2,1) // this will delete the 2nd position elememnt
+// arr1.splice(2,1, '123') // it will replace the 2nd position element
+// console.log(arr1)
+
+//slice - means it will cut the array 
    //   [0          , 1       ,  2     ,3          ]
 // var arr=["AngularJS","Node.js","JQuery","Bootstrap"]  
 // // slice(starting index value will be taken, excluded the index)
@@ -171,9 +215,9 @@ console.log(arr.flat(Infinity))
 // var result=arr.slice(0,3);  
 // console.log(result);  
 
-//map() it willt ake the array ans use the same array to performa logic
+// //map() it willt ake the array ans use the same array to performa logic
 
 // var num = [1,2,3]
 // // expresseion from the num take each value put it in a variable a 
-// var result = num.map(a => a)
+// var result = num.map(a => a*a)
 // console.log(result)
